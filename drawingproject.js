5cdda281a18fde961d2d7dfc8f0bf939a360b90a
Owen Vandevelde <owen.vnade@gmail.com>
2016-10-10 11:36:23 -0700

    orgaized

--- drawingproject.js
+++ drawingproject.js
@@ -11,8 +11,9 @@ canvas.arc(400, 220, 110, 0, 2*Math.PI);//second scoop
 canvas.fillStyle = "SkyBlue";
 canvas.fill();
 
+//cherry
 canvas.beginPath();
-canvas.arc(490, 165, 35, 0, 2*Math.PI);//second scoop
+canvas.arc(490, 165, 35, 0, 2*Math.PI);
 canvas.fillStyle = "Crimson";
 canvas.fill();
 
@@ -22,6 +23,7 @@ canvas.bezierCurveTo(500, 135, 555, 100, 600, 120);
 canvas.fillStyle = "ForestGreen";
 canvas.fill();
 
+//lines on top scoop
 canvas.beginPath();
 canvas.moveTo(350, 10);
 canvas.bezierCurveTo(350, 70, 390, 120, 390, 110);
@@ -46,12 +48,14 @@ canvas.bezierCurveTo(450, 70, 500, 140, 500, 130);
 canvas.fillStyle = "DarkGreen";
 canvas.fill();
 
+//first scoop
 canvas.beginPath();
-canvas.arc(400, 330, 110, 0, 2*Math.PI);//first scoop
+canvas.arc(400, 330, 110, 0, 2*Math.PI);
 canvas.fillStyle = "pink";
 canvas.fill();
 
-canvas.beginPath(); //cone
+//cone
+canvas.beginPath();
 canvas.moveTo(520,350); //upper right
 canvas.lineTo(280,350); //upper left
 canvas.lineTo(400,750); //bottom
@@ -68,7 +72,8 @@ canvas.fillRect(380, 280, 20, 5);
 canvas.fillStyle = "SaddleBrown";
 canvas.fill();
 
-canvas.beginPath();  //waffle cone
+//pattern on cone
+canvas.beginPath();
 canvas.moveTo(300,350);
 canvas.lineTo(420,750);
 canvas.fillStyle = "red";
@@ -104,7 +109,7 @@ canvas.lineTo(620,750);
 canvas.fillStyle = "red";
 canvas.stroke();
 
-canvas.beginPath();  //waffle cone other way
+canvas.beginPath();
 canvas.moveTo(500,350);
 canvas.lineTo(380,750);
 canvas.fillStyle = "red";
@@ -140,16 +145,17 @@ canvas.lineTo(180,750);
 canvas.fillStyle = "red";
 canvas.stroke();
 
-canvas.beginPath(); //line cover
+//line-pattern cover
+canvas.beginPath();
 canvas.moveTo(520,350); //upper
 canvas.lineTo(800,750); //lowwer right
 canvas.lineTo(400,750); //lowwer left
 canvas.fillStyle = "Cornsilk";
 canvas.fill();
 
-canvas.beginPath(); //line cover  other way
+canvas.beginPath();
 canvas.moveTo(280,350); //upper
-canvas.lineTo(179,750); //upper left
-canvas.lineTo(400,750); //bottom
+canvas.lineTo(179,750); //lowwer left
+canvas.lineTo(400,750); //lowwer right
 canvas.fillStyle = "Cornsilk";
 canvas.fill();
